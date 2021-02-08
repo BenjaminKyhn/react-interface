5c50ca391b47c27dc0bec5f1ddf384b2ebe68868
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-08 18:53:21 +0100

    2.2 complete

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import '../css/App.css';
+import React, {Component} from 'react';
+import AddAppointments from './AddAppointments';
+import SearchAppointments from "./SearchAppointments";
+import ListAppointments from "./ListAppointments";
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            myName: 'Ben'
+        }
+    }
+
+    render() {
+        return (
+            <main className="page bg-white" id="petratings">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 bg-white">
+                            <div className="container">
+                                {this.state.myName}
+                                <AddAppointments/>
+                                <SearchAppointments/>
+                                <ListAppointments/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        );
+    }
+
+}
+
+export default App;
