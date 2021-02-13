64a0d8ad0d52b64c8beab21a7849c1322b74ec80
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-13 17:03:34 +0100

    4.1 complete

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ class App extends Component {
             myAppointments: [],
             formDisplay: false,
             lastIndex: 0,
+            orderBy: 'petName',
+            orderDir: 'asc'
         }
         this.deleteAppointment = this.deleteAppointment.bind(this);
         this.toggleForm = this.toggleForm.bind(this);
@@ -58,6 +60,21 @@ class App extends Component {
     }
 
     render() {
+        let order;
+        let filteredApts = this.state.myAppointments;
+        if (this.state.orderDir === 'asc'){
+            order = 1;
+        } else {
+            order = -1;
+        }
+
+        filteredApts.sort((a, b) => {
+            if(a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase()){
+                return -1 * order;
+            } else {
+                return 1 * order;
+            }
+        })
 
         return (
             <main className="page bg-white" id="petratings">
@@ -72,7 +89,7 @@ class App extends Component {
                                 />
                                 <SearchAppointments/>
                                 <ListAppointments
-                                    appointments={this.state.myAppointments}
+                                    appointments={filteredApts}
                                     deleteAppointment={this.deleteAppointment}
                                 />
                             </div>
